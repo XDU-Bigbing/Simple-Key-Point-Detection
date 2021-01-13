61bd65b65b126e7ca0ab1510be652becb65450fb
muyuuuu <muyu0223@outlook.com>
2021-01-13 13:50:45 +0800

    fix up load data bug :bug:

--- code/train.py
+++ code/train.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
     
     # 获取有多少数据
     data_num = utils.get_data_num(JSONPATH)
+    print("Total data number is {}".format(data_num))
     
     # 最开始创建时覆盖原来文件
     with open(OUTFILE, 'w') as f:
-        f.write("Total data number is {} \n".format(data_num))
+        f.write("Total data number is {}".format(data_num))
 
     # 数据划分，60%训练，20%测试，20%验证
     train_scale, test_scale, valid_scale = 0.6, 0.2, 0.2
@@ -27,6 +28,10 @@ if __name__ == "__main__":
         test_scale=test_scale, 
         valid_scale=valid_scale)
     
+    print("Train data number is {}".format(train_num))
+    print("Test data number is {}".format(test_num))
+    print("Valid data number is {}".format(valid_num))
+    
     # 9138, 3046, 3046
     # print(train_num, test_num, valid_num, sep=', ')
     # 之后写文件就追加
@@ -36,9 +41,11 @@ if __name__ == "__main__":
         f.write("Valid data number is {} \n".format(valid_num))
 
     # 加载所有数据集
+    print('Beginning to load train data......')
     DATA = Loader(sum_num=data_num, 
         data_path=DATAPATH,
         json_path=JSONPATH,)
+    print('Train data has been loaded')
 
     # 一批训练的数据量
     batchsz = 16
--- code/utils.py
+++ code/utils.py
@@ -15,6 +15,7 @@ def get_train_test_valid_num(**kwargs):
     计算训练集、测试集合的数量
     '''
     # int 防止出现浮点数
+    data_num = kwargs['data_num']
     train_num = int(data_num * kwargs['train_scale'])
     test_num = int(data_num * kwargs['test_scale'])
     valid_num = int(data_num * kwargs['valid_scale'])
